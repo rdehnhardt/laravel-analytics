70d19623c0c576770fd6333197b398ffea587802
rdehnhardt <renatotkd@gmail.com>
2015-07-28 17:19:34 -0300

    Add helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -3,6 +3,11 @@
 use Carbon\Carbon;
 
 if (!function_exists('get_visits')) {
+    /**
+     * @param $startDate
+     * @param $endDate
+     * @return mixed
+     */
     function get_visits($startDate, $endDate)
     {
         $Visits = app()->make(\Baconfy\Analytics\Services\Visits\GetVisitByPeriod::class);
@@ -12,6 +17,12 @@ if (!function_exists('get_visits')) {
 }
 
 if (!function_exists('graph_values')) {
+    /**
+     * @param $records
+     * @param $key
+     * @param bool|false $withComma
+     * @return bool|string
+     */
     function graph_values($records, $key, $withComma = false)
     {
         $tmp = [];
