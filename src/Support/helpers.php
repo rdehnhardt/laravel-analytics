0055ba535954865113e127aafa45592603d51cdf
rdehnhardt <renatotkd@gmail.com>
2015-07-28 12:29:47 -0300

    Add helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
 if (!function_exists('get_visits')) {
+    /**
+     * Get visits by date
+     *
+     * @param $startDate
+     * @param $endDate
+     * @return mixed
+     */
     function get_visits($startDate, $endDate)
     {
         $Visits = app()->make(\Baconfy\Analytics\Services\Visits::class);
