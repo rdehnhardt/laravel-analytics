a0b79c30e10a5b1d04a7d08721e2d7b5f080dc81
Renato Dehnhardt <renatotkd@gmail.com>
2015-08-11 02:26:34 -0300

    Add referer

--- src/Services/Visits/CreateVisit.php
+++ src/Services/Visits/CreateVisit.php
@@ -12,14 +12,16 @@ class CreateVisit
      * @param $uuid
      * @param $ip
      * @param $location
+     * @param $referer
      * @return mixed
      */
-    public function fire($uuid, $ip, $location)
+    public function fire($uuid, $ip, $location, $referer)
     {
         return DB::table('analytcs_visits')->insert([
             'uuid' => $uuid,
             'location' => $location,
             'ip' => $ip,
+            'referer' => $referer,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
